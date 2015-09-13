4cd96a58223e9036927922dca5daf7998a1c0c11
Matt Salmon <mrsalmon@gmail.com>
2015-09-13 06:50:30 +0200

    Pushing UI file renames SourceTree somehow missed.

---
+++ source/Stateless.WorkflowEngine.UI.Console/AppCode/Models/Workflow/UIWorkflow.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stateless.WorkflowEngine.UI.Console.Models.Workflow
+{
+    [BsonIgnoreExtraElements]
+    public class UIWorkflow
+    {
+        [BsonElement("_id")]
+        public Guid Id { get; set; }
+
+        public int RetryCount { get; set; }
+
+        public int[] RetryIntervals { get; set; }
+
+        public string RetryIntervalsFriendly
+        {
+            get
+            {
+                return String.Join(",", this.RetryIntervals);
+            }
+        }
+
+        public DateTime? ResumeOn { get; set; }
+
+        public string ResumeOnFriendly
+        {
+            get
+            {
+                if (!ResumeOn.HasValue || ResumeOn.Value < new DateTime(1900, 1, 1))
+                {
+                    return "-";
+                }
+                return this.ResumeOn.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+        }
+
+        public bool IsSuspended { get; set; }
+    }
+}
---
+++ source/Stateless.WorkflowEngine.UI.Console/AppCode/Models/Workflow/UIWorkflowContainer.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stateless.WorkflowEngine.UI.Console.Models.Workflow
+{
+    [BsonIgnoreExtraElements] 
+    public class UIWorkflowContainer
+    {
+        public string WorkflowType { get; set; }
+
+        public string WorkflowTypeName
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(this.WorkflowType))
+                {
+                    return String.Empty;
+                }
+
+                ParsedAssemblyQualifiedName.ParsedAssemblyQualifiedName p = new ParsedAssemblyQualifiedName.ParsedAssemblyQualifiedName(this.WorkflowType);
+                return p.TypeName;
+
+            }
+        }
+
+        public UIWorkflow Workflow { get; set; }
+    }
+}
