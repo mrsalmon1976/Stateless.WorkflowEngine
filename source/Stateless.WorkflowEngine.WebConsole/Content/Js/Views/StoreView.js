2557437b3bbc61478841fcec2a4489414e0de534
Matt Salmon <mrsalmon@gmail.com>
2016-11-26 23:01:03 +0200

    #18 Added ability to suspend/unsuspend multiple workflows

--- source/Stateless.WorkflowEngine.WebConsole/Content/Js/Views/StoreView.js
+++ source/Stateless.WorkflowEngine.WebConsole/Content/Js/Views/StoreView.js
@@ -5,6 +5,18 @@ var StoreView = function () {
 
     this.workflowViewModel = null;
 
+    this.getSelectedWorkflowIds = function () {
+        var selectedBoxes = $('.chk-workflow:checked');
+        var workflowIds = [];
+
+        selectedBoxes.each(function () {
+            var workflowId = $(this).val();
+            workflowIds.push(workflowId);
+        });
+
+        return workflowIds;
+    };
+
     this.init = function () {
         $('#btn-refresh').on('click', function () { that.loadWorkflows(); });
         $('#btn-suspend').on('click', function () { that.workflowViewModel.toggleWorkflowSuspension(); });
@@ -14,9 +26,14 @@ var StoreView = function () {
     this.loadWorkflows = function () {
 
         var connId = $('#pnl-workflows').data().modelId;
-        $('a.workflow-id').off('click');    // remove row event handlers
         $('#pnl-loading').show();
         $('#pnl-workflows').html('');
+
+        // remove event handlers
+        $('a.workflow-id').off('click');
+        $('#chk-workflow-all').off('click');
+        $('#btn-suspend').off('click');
+        $('#btn-unsuspend').off('click');
 
         var request = $.ajax({
             url: "/store/list",
@@ -27,8 +44,11 @@ var StoreView = function () {
 
         request.done(function (response) {
             $('#pnl-workflows').html(response);
-            // attach row event handlers again
+            // attach event handlers again
             $('a.workflow-id').on('click', function () { that.openWorkflowDialog($(this)); });
+            $('#chk-workflow-all').on('click', function () { that.toggleWorkflowCheckboxes($(this)); });
+            $('#btn-suspend').on('click', function () { that.suspendWorkflows(); });
+            $('#btn-unsuspend').on('click', function () { that.unsuspendWorkflows(); });
         });
 
         request.fail(function (xhr, textStatus, errorThrown) {
@@ -46,6 +66,82 @@ var StoreView = function () {
             that.workflowViewModel = new StoreViewWorkflowViewModel();
         }
         that.workflowViewModel.showDialog(evtSource.data('id'), evtSource.data().type, function () { that.loadWorkflows(); });
+    };
+
+    this.submitWorkflowActions = function (workflowIds, target) {
+        
+        var model = {
+            "WorkflowIds": workflowIds,
+            "ConnectionId": $('#pnl-workflows').data().modelId
+        };
+
+        var request = $.ajax({
+            url: target,
+            method: "POST",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(model)
+        });
+        request.done(function (response) {
+            that.loadWorkflows();
+        });
+    };
+
+    this.suspendWorkflows = function () {
+        var workflowIds = this.getSelectedWorkflowIds();
+        if (workflowIds.length == 0) {
+            return;
+        }
+        bootbox.confirm({
+            message: "Are you sure you want to suspend all selected workflows?",
+            buttons: {
+                confirm: {
+                    label: ' Yes ',
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: ' No ',
+                    className: 'btn-default'
+                }
+            },
+            callback: function (result) {
+                if (result === true) {
+                    that.submitWorkflowActions(workflowIds, '/store/suspend');
+                }
+            }
+        });
+    };
+
+    this.toggleWorkflowCheckboxes = function (evtSource) {
+        var checked = evtSource.is(':checked');
+        $(".chk-workflow").each(function () {
+            $(this).prop("checked", checked);
+        });
+    };
+
+    this.unsuspendWorkflows = function () {
+        var workflowIds = this.getSelectedWorkflowIds();
+        if (workflowIds.length == 0) {
+            return;
+        }
+        bootbox.confirm({
+            message: "Are you sure you want to unsuspend all selected workflows?",
+            buttons: {
+                confirm: {
+                    label: ' Yes ',
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: ' No ',
+                    className: 'btn-default'
+                }
+            },
+            callback: function (result) {
+                if (result === true) {
+                    that.submitWorkflowActions(workflowIds, '/store/unsuspend');
+                }
+            }
+        });
     };
 
 };
