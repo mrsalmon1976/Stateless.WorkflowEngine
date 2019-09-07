ff976ffbb33f906b32651348f84c028b6928dbe0
Matt Salmon <matt@iua.co.za>
2019-09-07 22:31:56 +0200

    Updated build script with dotnet publish for MongoDb release

--- deployment/build.ps1
+++ deployment/build.ps1
@@ -25,9 +25,14 @@ $root = $PSScriptRoot
 $source = $root.Replace("deployment", "") + "\source"
 $version = Read-Host -Prompt "What version are we building? [e.g. 1.5.0]"
 
-# build for mongo db 
-Write-Host "Building Stateless.Workflow.MongoDB version $version"
-$mongodb = "$source\Stateless.WorkflowEngine.MongoDb\bin\Release"
+# publish mongo db (dotnet standard)
+Set-Location "$source\Stateless.WorkflowEngine.MongoDb" 
+Write-Host "Publishing Stateless.Workflow.MongoDB version $version"
+dotnet clean -c Release
+dotnet publish -c Release
+Set-Location $root
+
+$mongodb = "$source\Stateless.WorkflowEngine.MongoDb\bin\Release\netstandard2.0\publish"
 $zip = "$root\Stateless.WorkflowEngine.MongoDb_v$version.zip"
 [system.io.file]::Delete($zip)
 ZipFile -sourcefile "$mongodb\*.*" -zipfile $zip 
