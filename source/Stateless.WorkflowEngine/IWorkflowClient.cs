a12f1d9704df792a3d18a019dc205037595a90f7
Matt Salmon <mrsalmon@gmail.com>
2026-03-07 11:04:12 +0200

    Moved IWorkflowClient and IWorkflowServer into their own files

---
+++ source/Stateless.WorkflowEngine/IWorkflowClient.cs
@@ -0,0 +1,86 @@
+using Stateless.WorkflowEngine.Stores;
+using System;
+
+namespace Stateless.WorkflowEngine
+{
+    public interface IWorkflowClient
+    {
+
+        /// <summary>
+        /// Gets/sets the workflow store attached to the workflow server.
+        /// </summary>
+        IWorkflowStore WorkflowStore { get; set; }
+
+        /// <summary>
+        /// Deletes a workflow from the underlying store.  This checks workflows in the active store
+        /// only, not in the underlying Completed collection.
+        /// </summary>
+        /// <param name="workflowId"></param>
+        void Delete(Guid workflowId);
+
+        /// <summary>
+        /// Gets whether a workflow still exists or not.
+        /// </summary>
+        /// <param name="workflowId"></param>
+        /// <returns></returns>
+        bool Exists(Guid workflowId);
+
+        /// <summary>
+        /// Gets a workflow from the back-end store.  Returns null if the workflow does not exist.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="workflowId"></param>
+        /// <returns></returns>
+        T Get<T>(Guid workflowId) where T : Workflow;
+
+        /// <summary>
+        /// Gets the count of workflows on the underlying store (including suspended).
+        /// </summary>
+        /// <returns></returns>
+        long GetIncompleteCount();
+
+        /// <summary>
+        /// Gets the count of completed workflows on the underlying store.
+        /// </summary>
+        /// <returns></returns>
+        long GetCompletedCount();
+
+        /// <summary>
+        /// Gets the count of suspended workflows that have not completed on the underlying store.
+        /// </summary>
+        /// <returns></returns>
+        long GetSuspendedCount();
+
+        /// <summary>
+        /// Checks to see if a single-instance workflow has already been registered.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        bool IsSingleInstanceWorkflowRegistered<T>() where T : Workflow;
+
+        /// <summary>
+        /// Registers a new workflow with the engine.  Single instance workflows that already exist will result in
+        /// an exception being raised.
+        /// </summary>
+        /// <param name="workflow">The workflow.</param>
+        /// <returns>True if a new workflow was started, otherwise false.</returns>
+        void Register(Workflow workflow);
+
+        /// <summary>
+        /// Registers a new workflow with the engine.  Single instance workflows that already exist will result in
+        /// an exception being raised.
+        /// </summary>
+        /// <param name="workflow">The workflow.</param>
+        /// <returns>True if a new workflow was started, otherwise false.</returns>
+        [Obsolete("Use Register instead")]
+        void RegisterWorkflow(Workflow workflow);
+
+        /// <summary>
+        /// Unsuspends a workflow.
+        /// </summary>
+        /// <param name="workflowId"></param>
+        /// <returns></returns>
+        Workflow Unsuspend(Guid workflowId);
+
+    }
+}
---
+++ source/Stateless.WorkflowEngine/IWorkflowServer.cs
@@ -0,0 +1,90 @@
+using Stateless.WorkflowEngine.Events;
+using Stateless.WorkflowEngine.Stores;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stateless.WorkflowEngine
+{
+    public interface IWorkflowServer
+    {
+        /// <summary>
+        /// Gets/sets the resolver used to instantiate new instances of classes required for workflow execution.
+        /// This defaults to null, in which case classes are created with reflection.  Setting this property
+        /// to your own resolver will allow you to control how workflow actions are created.
+        /// </summary>
+        IWorkflowEngineDependencyResolver DependencyResolver { get; set; }
+
+        /// <summary>
+        /// Gets/sets the options applicable to the workflow server.
+        /// </summary>
+        WorkflowServerOptions Options { get; set; }
+
+        /// <summary>
+        /// Gets/sets the workflow store attached to the workflow server.
+        /// </summary>
+        IWorkflowStore WorkflowStore { get; set; }
+
+
+        /// <summary>
+        /// Executes a workflow.
+        /// </summary>
+        /// <param name="workflow"></param>
+        void ExecuteWorkflow(Workflow workflow);
+
+        /// <summary>
+        /// Executes a workflow.
+        /// </summary>
+        /// <param name="workflow"></param>
+        Task ExecuteWorkflowAsync(Workflow workflow);
+
+        /// <summary>
+        /// Executes the first <c>count</c> workflows in the registered store, ordered by Priority DESC, RetryCount DESC, and then
+        /// by CreationDate.  You can optionally elect to specify the number of workflows that should execute in parallel.
+        /// </summary>
+        /// <param name="count">The number of active workflows to be loaded for processing.</param>
+        /// <param name="maxConcurrent">The maximum number of workflows to processing parallel - defaults to the value of <c>count</c>.</param>
+        /// <returns>The number of workflows that were actually executed.</returns>
+        int ExecuteWorkflows(int count, int? maxConcurrent = null);
+
+        /// <summary>
+        /// Executes the first <c>count</c> workflows in the registered store, ordered by Priority DESC, RetryCount DESC, and then
+        /// by CreationDate.  You can optionally elect to specify the number of workflows that should execute in parallel.
+        /// </summary>
+        /// <param name="count">The number of active workflows to be loaded for processing.</param>
+        /// <param name="maxConcurrent">The maximum number of workflows to processing parallel - defaults to the value of <c>count</c>.</param>
+        /// <returns>The number of workflows that were actually executed.</returns>
+        Task<int> ExecuteWorkflowsAsync(int count, int? maxConcurrent = null, CancellationToken? cancellationToken = null);
+
+        /// <summary>
+        /// Checks to see if a single-instance workflow has already been registered.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        bool IsSingleInstanceWorkflowRegistered<T>() where T : Workflow;
+
+        /// <summary>
+        /// Registers a new workflow with the engine.  Single instance workflows that already exist will result in
+        /// an exception being raised.
+        /// </summary>
+        /// <param name="workflow">The workflow.</param>
+        void RegisterWorkflow(Workflow workflow);
+
+        /// <summary>
+        /// Registers a workflow type for processing.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        void RegisterWorkflowType<T>() where T : Workflow;
+
+        /// <summary>
+        /// Event raised when a workflow is suspended.
+        /// </summary>
+        event EventHandler<WorkflowEventArgs> WorkflowSuspended;
+
+        /// <summary>
+        /// Event raised when a workflow completes.
+        /// </summary>
+        event EventHandler<WorkflowEventArgs> WorkflowCompleted;
+
+    }
+}
